c3e356877006ca9447f3fb9d52c83dcae3e7bd67
Abdullah Albrawy <48991028+sargaharrey@users.noreply.github.com>
2023-01-22 03:25:05 -0800

    'main'

--- app.js
+++ app.js
@@ -1,128 +1,221 @@
-require("dotenv").config();
-require("./config/database").connect();
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
-const User = require("./model/user");
-const auth = require("./middleware/auth");
-
+const express = require('express');
 const app = express();
+const multer = require('multer');
+const fs = require('fs');
 
-app.use(express.json({ limit: "50mb" }));
 
-app.post("/register", async (req, res) => {
-  try {
-    // Get user input
-    const { first_name, last_name, email, password } = req.body;
+const upload = multer();
 
-    // Validate user input
-    if (!(email && password && first_name && last_name)) {
-      res.status(400).send("All input is required");
-    }
+app.set('view engine', 'ejs');
 
-    // check if user already exist
-    // Validate if user exist in our database
-    const oldUser = await User.findOne({ email });
+const { google } = require('googleapis');
+const dialogflow = google.dialogflow('v2');
 
-    if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
-    }
+const key = require('./fourth.json')
+// Create a JWT client
+const jwtClient = new google.auth.JWT(
+  key.client_email,
+  null,
+  key.private_key,
+  ['https://www.googleapis.com/auth/dialogflow']
+  //   { keyFile: './fourth.json'},
+  //     ['https://www.googleapis.com/auth/dialogflow']
+);
 
-    //Encrypt user password
-    const encryptedPassword = await bcrypt.hash(password, 10);
+const auth = new google.auth.JWT(
+  key.client_email,
+  null,
+  key.private_key,
+  ['https://www.googleapis.com/auth/dialogflow']
 
-    // Create user in our database
-    const user = await User.create({
-      first_name,
-      last_name,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
-      password: encryptedPassword,
-    });
 
-    // Create token
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h",
-      }
-    );
-    // save user token
-    user.token = token;
 
-    // return new user
-    res.status(201).json(user);
-  } catch (err) {
+);
+
+
+
+authIDs = ['./newagent-9sfy-0cdcd6bfc506.json', './fourth.json', './fvf-rsbn-390b279cfdad.json']
+// Authenticate the JWT client
+jwtClient.authorize((err, tokens) => {
+  if (err) {
     console.log(err);
+    return;
   }
+})
+
+
+var responses = []
+  ;
+// Use the JWT client to create a Dialogflow client
+// const auth = new google.dialogflow({ version: 'v2', auth: jwtClient });
+
+const projectId = 'oversoft-35450';
+//  const client = new dialogflow.AgentsClient({
+//      projectId: projectId,
+//        keyFilename: './fourth.json',
+//      });
+
+// const intentsClient =new client.IntentsClient();
+// const entitiesClient =new  client.EntityTypesClient();
+
+
+
+// app.get('/', async (req, res) => {
+//     // const request = {
+//     //     parent: intentsClient.projectAgentPath(projectId),
+//     // };
+
+//     const responses = await dialogflow.projects.agent.intents.list({
+//         auth: auth,
+//         parent: `projects/${projectId}/agent`
+//     })
+
+//     const intents = responses.data.intents;
+//     console.log(intents)
+
+//     res.render('intents', { intents });
+
+// });
+
+app.post('/upload', upload.single('jsonFile'), async (req, res) => {
+
+
+
+  const auth = new google.auth.JWT(
+    req.file.path.client_email,
+    null,
+    req.file.path.private_key,
+    ['https://www.googleapis.com/auth/dialogflow']
+  );
+  authIDs.push(req.file.path)
+  //  responses.push(await dialogflow.projects.agent.entityTypes.list({
+  //     auth: auth,
+  //     parent: `projects/${req.file.path.project_id}/agent`
+  // }))
+
+  res.redirect('/');
+
+});
+app.get('/', async (req, res) => {
+  // const request = {
+  //     parent: intentsClient.projectAgentPath(projectId),
+  // };
+
+
+
+  // const intents = responses.data.intents;
+  // console.log(intents)
+  // res.render('intents', projectId);
+  projectIds = ['newagent-9sfy', 'oversoft-35450', 'fvf-rsbn']
+  res.render('projects', projectIds)
+
 });
 
-app.post("/login", async (req, res) => {
-  try {
-    // Get user input
-    const { email, password } = req.body;
 
-    // Validate user input
-    if (!(email && password)) {
-      res.status(400).send("All input is required");
-    }
-    // Validate if user exist in our database
-    const user = await User.findOne({ email });
+app.get('/projects/:Id', async (req, res) => {
+  // const request = {
+  //     parent: intentsClient.projectAgentPath(projectId),
+  // };
+  const projectPath = req.params.Id
+  console.log(authIDs[projectPath])
+  const key = require(authIDs[projectPath])
+  const jwtClient = new google.auth.JWT(
+    key.client_email,
+    null,
+    key.private_key,
+    ['https://www.googleapis.com/auth/dialogflow']
+    //   { keyFile: './fourth.json'},
+    //     ['https://www.googleapis.com/auth/dialogflow']
+  );
+  const entitesResponses = await dialogflow.projects.agent.entityTypes.list({
+    auth: jwtClient,
+    parent: `projects/${key.project_id}/agent`
+  })
+  const intntsResponses = await dialogflow.projects.agent.intents.list({
+    auth: jwtClient,
+    parent: `projects/${key.project_id}/agent`
+  })
+  //  const intents = responses.data.intents;
+  // // console.log(intents)
+  // // res.render('intents', projectId);
+  // projectIds = ['newagent-9sfy', 'oversoft-35450', 'fvf-rsbn']
 
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
 
-      // save user token
-      user.token = token;
 
-      // user
-      res.status(200).json(user);
-    }
-    res.status(400).send("Invalid Credentials");
-  } catch (err) {
-    console.log(err);
-  }
+  //  const entities = entitesResponses.data.entityTypes;
+  // console.log(entities)
+  // res.render('entities', { entities });
+
+  // const intities = intntsResponses
+
+
+
+  // intentsClient
+  //     .batchUpdateIntents(request2)
+  //     .then(responses => {
+  //         console.log(`Intents updated: ${responses[0]}`);
+  //         const getIntentRequest = { name: responses[0] };
+  //         // Call the getIntent method
+  //         console.log('==============================================================================================', intentsClient.getIntent(getIntentRequest));
+  //     })
+  //     .catch(err => {
+  //         console.error(`Error updating intents: ${err}`);
+  //     });
+  // res.redirect('/')
+
+  const data = [entitesResponses.data.entityTypes, intntsResponses.data.intents]
+  // console.log(data[0].entityTypes, data[1].intents)   
+  console.log(data[0][0].displayName, data[1][00].displayName)
+  res.render('index.ejs', { data })
+
+
 });
 
-app.get("/welcome", auth, (req, res) => {
-  res.status(200).send("Welcome 🙌 ");
+// app.get('/entities', async (req, res) => {
+
+//     const responses = await dialogflow.projects.agent.entityTypes.list({
+//         auth: auth,
+//         parent: `projects/${projectId}/agent`
+//     })
+
+//     const entities = responses.data.entityTypes;
+//     console.log(entities)
+//     res.render('entities', { entities });
+// });
+
+
+// app.get('/customize', async (req, res) => {
+
+
+//     // Get the updated entities and intents from the request body
+//     const { entities } = req.query.entities;
+//     const { intents } = req.query.intents;
+
+
+//     const intentsClient = new dialogflow.IntentsClient();
+
+
+//     // Create the request
+//     const request2 = {
+//         parent: `projects/${projectId}/agent`,
+//         intents: { aa: 'aaa' }
+//     };
+
+
+//     intentsClient
+//         .batchUpdateIntents(request2)
+//         .then(responses => {
+//             console.log(`Intents updated: ${responses[0]}`);
+//             const getIntentRequest = { name: responses[0] };
+//             // Call the getIntent method
+//             console.log('==============================================================================================', intentsClient.getIntent(getIntentRequest));
+//         })
+//         .catch(err => {
+//             console.error(`Error updating intents: ${err}`);
+//         });
+//         res.redirect('/')
+// });
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
-
-// This should be the last route else any after it won't work
-app.use("*", (req, res) => {
-  res.status(404).json({
-    success: "false",
-    message: "Page not found",
-    error: {
-      statusCode: 404,
-      message: "You reached a route that is not defined on this server",
-    },
-  });
-});
-
-module.exports = app;
-
-
-const { JWT } = require('google-auth-library');
-const keys = require('./oversoft-35450-d7d5ee0caa69.json');
-
-async function main() {
-    const client = new JWT({
-        email: keys.client_email,
-        key: keys.private_key,
-        scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    });
-    const url = `https://dns.googleapis.com/dns/v1/projects/${keys.project_id}`;
-    const res = await client.request({ url });
-    console.log(res.data);
-}
-
-main().catch(console.error);
