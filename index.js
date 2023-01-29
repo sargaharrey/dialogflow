53ddd742a0a88de1d679f73480969812f69f8370
Abdullah Albrawy <48991028+sargaharrey@users.noreply.github.com>
2023-01-29 03:45:17 -0800

    'mani'

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const multer = require('multer');
 const fs = require('fs');
 var fuzzy = require('fuzzy');
-
+var param= []
 var entitiesArr = []
 var intentsArr = []
-
+var names = []
+var df = []
 var data = []
 // Set multer file storage folder
 const upload = multer({ dest: '/' })
@@ -37,7 +38,8 @@ const auth = new google.auth.JWT(
 );
 var url = require('url');
 const { iam } = require('googleapis/build/src/apis/iam');
-
+const { dfareporting } = require('googleapis/build/src/apis/dfareporting');
+const { dirname } = require('path');
 
 
 authIDs = ['./newagent-9sfy-0cdcd6bfc506.json', './fourth.json', './fvf-rsbn-390b279cfdad.json']
@@ -59,6 +61,8 @@ var responses = []
 // const auth = new google.dialogflow({ version: 'v2', auth: jwtClient });
 
 const projectId = 'oversoft-35450';
+var parameter  = []
+var files = []
 //  const client = new dialogflow.AgentsClient({
 //      projectId: projectId,
 //        keyFilename: './fourth.json',
@@ -94,60 +98,101 @@ app.get('/', async (req, res) => {
     // const intents = responses.data.intents;
     // console.log(intents)
     // res.render('intents', projectId);
-    projectIds = authIDs.map(item => ({ item: item, id: require(item).project_id }))
-    console.log(projectIds)
-    res.render('projects', projectIds)
+    // projectIds = authIDs.map(item => ({ item: item, id: require(item).project_id }))
+    // console.log(projectIds)
+    // res.render('projects', projectIds)
+    // console.log(req)
+    //  const file = require(`./${req.url.split('upload=')[1].substr(0,req.url.split('=')[1].length-5)}`)
+    // const file = require(req.query.upload)
+    // console.log(req)
+
+    // // // console.log('======================', `./${req.url.split('=')[1]}`)
+    // const auth = new google.auth.JWT(
+    //     file.client_email,
+    //     null,
+    //     file.private_key,
+    //     ['https://www.googleapis.com/auth/dialogflow']
+    // );
+    // dialogflow.projects.agent.entityTypes.list({
+    //     auth: auth,
+    //     parent: `projects/${file.project_id}/agent`
+    // }).then(item =>
+    //     responses.push(item)
+    // )
+    // // console.log(authIDs) 
+
+    // // authIDs.push(`./${req.url.split('=')[1]}`) 
+    // names.push(req.parameter)
+    // for (let i = 0; i < names; i++) {
+    //     df.push([names[i], [responses]])
+    // }   
+
+    res.render('projects', { df, files, param, parameter })
+    // console.log(auth)
 
 });
 
+app.get('/add', async (req, res) => {
+    const file = require(`./${req.query.upload}`)
+    console.log(file)
 
-
-app.get('/upload',  async (req, res) => {
-
-    // console.log(req)
-    const file = require(`./${req.url.split('=')[1].substr(0,req.url.split('=')[1].length-5)}`)
-    // console.log('======================', `./${req.url.split('=')[1]}`)
-    const auth = new google.auth.JWT(
-      file.client_email,
-      null,
-      file.private_key,
-      ['https://www.googleapis.com/auth/dialogflow']
-    );
-    dialogflow.projects.agent.entityTypes.list({
-        auth: auth,
-         parent: `projects/${file.project_id}/agent`
-    }).then( item=> 
-     responses.push(item)
-            )
+    // // console.log('======================', `./${req.url.split('=')[1]}`)
+    // const auth = new google.auth.JWT(
+    //     file.client_email,
+    //     null,
+    //     file.private_key,
+    //     ['https://www.googleapis.com/auth/dialogflow']
+    // );
+    // dialogflow.projects.agent.entityTypes.list({
+    //     auth: auth,
+    //     parent: `projects/${file.project_id}/agent`
+    // }).then(item =>
+    //     responses.push(item)
+    // )
     // console.log(authIDs) 
-    
-    authIDs.push(`./${req.url.split('=')[1]}`) 
-    // console.log(auth)
-    res.redirect('/');
+
+    // authIDs.push(`./${req.url.split('=')[1]}`) 
+  
+   
+        // names.push(req.query.parameter)
+         df.push(req.query.df)
+     files.push(file)
+    // res.redirect('/')
+// console.log(df)
+
+    res.redirect('/')
+
 
 })
 
-app.get('/search', async (req, res) => {
-   
+
+// app.get('/add',  async (req, res) => {
+
+ 
+
+// })
+
+app.get('/compare', async (req, res) => {
+    // const file =`${req.query.parameters}`
+
     const regex = new RegExp(`projects=(.*?)(&)`);
-    var searchParameter = req.url.split('parameter=')[1];
-    console.log(searchParameter)
+    // var searchParameter = req.url.split('parameter=')[1];
+    // console.log(searchParameter)
     console.log(req.query.parameters )
     var files = req.url.split(regex);
-    var DFS = files.filter(i => i != '' && i != '&')
+    //  var DFS = files.filter(i => i != '' && i != '&')
 
-    var DFSFiles = authIDs.filter(item => DFS.map(project => item.includes(project) ))
+    // var DFSFiles = authIDs.filter(item => DFS.map(project => item.includes(project) ))
 
-    var DFSFilesRoutes = DFSFiles.map(item => require(item))
-    console.log(DFSFilesRoutes[0])
+    //  var DFSFilesRoutes = DFSFiles.map(item => require(item))
+    // console.log(DFSFilesRoutes[0])
     // console.log('.............................', DFSFiles )
-    var projectIds = req.query.projects
-    console.log(projectIds)
-    var finalResult = []
-    var result = []
-    var result2 = []
+    // var projectIds = req.query.projects
+    // console.log(projectIds)
+    // var finalResult = []
 
-    console.log(projectIds)
+
+    // console.log(projectIds)
     // console.log(authIDs, projectIds) 
  
     // var url_parts = url.parse(req.url, true);
@@ -156,75 +201,78 @@ app.get('/search', async (req, res) => {
     const dialogflow = require('dialogflow');
  
     // console.log()
-    for (let i = 0; i < DFSFilesRoutes.length;i++){
+    // for (let i = 0; i < DFSFilesRoutes.length;i++){
         
-        // ids.push(DFSFilesRoutes[i])
-        // Set the project ID and auth credentials
-        // const projectId = 'oversoft-35450';
-        // const credentials = {
+    //     // ids.push(DFSFilesRoutes[i])
+    //     // Set the project ID and auth credentials
+    //     // const projectId = 'oversoft-35450';
+    //     // const credentials = {
 
-        // };
-        // console.log(DFSFilesRoutes[1].project_id)
+    //     // };
+    //     // console.log(DFSFilesRoutes[1].project_id)
 
-        // Create a new client
-        console.log('======================================================================')
-        const clientIntents = new dialogflow.v2.IntentsClient(
+    //     // Create a new client
+    //     // console.log('======================================================================')
+    //     // const clientIntents = new dialogflow.v2.IntentsClient(
 
-            { credentials:DFSFilesRoutes[i] }
-        );
-        const clientEntites = new dialogflow.v2.EntityTypesClient(
+    //     //     { credentials:DFSFilesRoutes[i] }
+    //     // );
+    //     // const clientEntites = new dialogflow.v2.EntityTypesClient(
 
-            { credentials: DFSFilesRoutes[i] }    
-        );
+    //     //     { credentials: DFSFilesRoutes[i] }    
+    //     // );
 
-        const agentPathIntent = clientIntents.projectAgentPath(`${DFSFilesRoutes[i].project_id}`);
-     const agentPathEntity = clientEntites.projectAgentPath(`${DFSFilesRoutes[i].project_id}`);
+    // //     const agentPathIntent = clientIntents.projectAgentPath(`${DFSFilesRoutes[i].project_id}`);
+    // //  const agentPathEntity = clientEntites.projectAgentPath(`${DFSFilesRoutes[i].project_id}`);
 
-        // , query: `displayName:${searchParameter}` 
-        clientIntents.listIntents({ parent: agentPathIntent, query:`name:${searchParameter}` })
-            .then(responses => {
-                 intentsArr.push(responses[0]);
-                // console.log('Intents:');
-              })
-            .catch(err => {
-                console.error('Failed to list intents:', err);
-            });
+    // //     // , query: `displayName:${searchParameter}` 
+    // //     clientIntents.listIntents({ parent: agentPathIntent})
+    // //         .then(responses => {
+    // //              intentsArr.push(responses[0]);
+    // //             // console.log('Intents:');
+    // //           })
+    // //         .catch(err => {
+    // //             console.error('Failed to list intents:', err);
+    // //         });
 
             
 
-        clientEntites.listEntityTypes({ parent: agentPathEntity, query: `name:${searchParameter}` })
-            .then(responses => {
-                entitiesArr.push(responses[0]);
-                // console.log('entities:');
+    // //     clientEntites.listEntityTypes({ parent: agentPathEntity})
+    // //         .then(responses => {
+    // //             entitiesArr.push(responses[0]);
+    // //             // console.log('entities:');
              
                 
-            }).catch(err => {
-                console.error('Failed to list intents:', err);
-            });
-        console.log(searchParameter.replace(/[+]/g, ' '))
+    // //         }).catch(err => {
+    // //             console.error('Failed to list intents:', err);
+    // //         });
 
-        // console.log(';;;;;;;;;;;;;;;;;;;;;',intents,entities)
-        // var dat = fuzzy.filter(`${searchParameter}`, intentsArr)     
+    //         // , filter: `${req.query.parameters}`
+    //         // , filter: `${req.query.parameters}` 
+    //     // console.log(searchParameter.replace(/[+]/g, ' '))
 
-        if (req.query.parameters === 'action') {
-            intentsArr.map( i => i.filter(item => item.action === searchParameter.replace(/[+]/g, ' ')))
-            entitiesArr.map(i => i.filter(item => item.action === searchParameter.replace(/[+]/g, ' '))) 
+    //     // console.log(';;;;;;;;;;;;;;;;;;;;;',intents,entities)
+    //     // var dat = fuzzy.filter(`${searchParameter}`, intentsArr)     
 
-        }
-        else if (req.query.parameters === 'name') {
+    //     // if (req.query.parameters === 'action') {
+    //     //     intentsArr.map( i => i.filter(item => item.action === searchParameter.replace(/[+]/g, ' ')))
+    //     //     entitiesArr.map(i => i.filter(item => item.action === searchParameter.replace(/[+]/g, ' '))) 
+
+    //     // }
+    //     // else if (req.query.parameters === 'name') {
            
 
-            intentsArr.map(i => i.filter(item => item.name === searchParameter.replace(/[+]/g, ' ')))
-            entitiesArr.map(i => i.filter(item => item.name === searchParameter.replace(/[+]/g, ' '))) 
-        }
-        else {
-            intentsArr.map(i => i.filter(item => item.displayName === searchParameter.replace(/[+]/g, ' ')))
-            entitiesArr.map(i => i.filter(item => item.displayName === searchParameter.replace(/[+]/g, ' '))) 
-            // result2= entitiesArr.filter(item => item.displayName === searchParameter.replace(/[+]/g, ' '))
-        }
-        finalResult.push([intentsArr, entitiesArr])
-        // console.log(finalResult.push([intentsArr, entitiesArr]))
-        }
+    //     //     intentsArr.map(i => i.filter(item => item.name === searchParameter.replace(/[+]/g, ' ')))
+    //     //     entitiesArr.map(i => i.filter(item => item.name === searchParameter.replace(/[+]/g, ' '))) 
+    //     // }
+    //     // else {
+    //     //     intentsArr.map(i => i.filter(item => item.displayName === searchParameter.replace(/[+]/g, ' ')))
+    //     //     entitiesArr.map(i => i.filter(item => item.displayName === searchParameter.replace(/[+]/g, ' '))) 
+    //     //     // result2= entitiesArr.filter(item => item.displayName === searchParameter.replace(/[+]/g, ' '))
+    //     // }
+    //     finalResult.push([intentsArr, entitiesArr])
+    //     // console.log(finalResult.push([intentsArr, entitiesArr]))
+    //     }
     // console.log(finalResult.map(i => i.flat(10).filter(a => a.length > 0)))
     // console.log(encodeURIComponent(searchParameter))
     // const filterd = Object.entries(intents).map(item => ite==)
@@ -240,11 +288,21 @@ app.get('/search', async (req, res) => {
     //  var finnal = [result, result2]
     // var finnal = result
 
-   const finalIntents =  finalResult[0].map( i=> i.filter(a => a.length > 0))
-    const finalEntites = finalResult[1].map(i => i.filter(a => a.length > 0))      
-    finalResulta = [finalIntents, finalEntites]
-    console.log(finalResult)
-    res.render('DataTables', { finalResulta, projectIds });
+//    const finalIntents =  finalResult[0].map( i=> i.filter(a => a.length > 0))
+//     const finalEntites = finalResult[1].map(i => i.filter(a => a.length > 0))      
+    // finalResulta = [finalIntents, finalEntites]
+    // console.log(finalResult)
+
+    const uu = req.query.parameters.split('.')
+
+
+    // data = finalResulta
+    // console.log('=============',data.map(i =>  i)[0].map(i4=> i4).flat(10))
+    console.log(uu)
+    param = uu
+    parameter.push(req.query.parameters)
+//     console.log(uu[0].uu[1].uu[2])
+    // res.render('DataTables', { finalResulta, projectIds });
     // console.log(req.query)
 
     // const auth = new google.auth.JWT(
@@ -260,7 +318,7 @@ app.get('/search', async (req, res) => {
     // // }))
     // console.log(authIDs)
     // console.log(auth)
-    // res.redirect('/');
+     res.redirect('/');
         
 });
 
@@ -338,69 +396,79 @@ app.get('/search', async (req, res) => {
 // });
 
 
-app.get(/customzie/ , async (req, res) => {
+// app.get(/customzie/ , async (req, res) => {
 
-    console.log(req.path.split('/')[2])
-    // Get the updated entities and intents from the request body
-    const { entity } = req.query;
-    const { intent} = req.query;
+//     console.log(req.path.split('/')[2])
+//     // Get the updated entities and intents from the request body
+//     const { entity } = req.query;
+//     const { intent} = req.query;
 
-    console.log(intent, entity)
-    const intentsClient = await dialogflow.projects.agent.intents
-    console.log(intentsClient)
-    const entitiesClient = await dialogflow.projects.agent.entityTypes
+//     console.log(intent, entity)
+//     const intentsClient = await dialogflow.projects.agent.intents
+//     console.log(intentsClient)
+//     const entitiesClient = await dialogflow.projects.agent.entityTypes
 
-    // Create the request
-    const request = {
-        name: `projects/${projectId}/agent/`,
-        requestBody: {
-                   // request body parameters
+//     // Create the request
+//     const request = {
+//         name: `projects/${projectId}/agent/`,
+//         requestBody: {
+//                    // request body parameters
                 
-                  "autoExpansionMode": "my_autoExpansionMode",
-                  "displayName": "my_displayName",
-                  "enableFuzzyExtraction": false,
-                  "entities": [],
-                  "kind": "my_kind",
-                  "name": "my_name"
+//                   "autoExpansionMode": "my_autoExpansionMode",
+//                   "displayName": "my_displayName",
+//                   "enableFuzzyExtraction": false,
+//                   "entities": [],
+//                   "kind": "my_kind",
+//                   "name": "my_name"
                 
-              },
-    };
-    const request2 = {
-        name: `projects/${projectId}/agent`,
-        intents: { entity: entity }
-    };
+//               },
+//     };
+//     const request2 = {
+//         name: `projects/${projectId}/agent`,
+//         intents: { entity: entity }
+//     };
 
 
-    if (intent) {
-    intentsClient
-        .patch(request)
-        // .then(responses => {
-        //     console.log(`Intents updated: ${responses[0]}`);
-        //     const getIntentRequest = { name: responses[0] };
-        //     // Call the getIntent method
-        //     console.log('==============================================================================================', intentsClient.getIntent(getIntentRequest));
-        // })
-        // .catch(err => {
-        //     console.error(`Error updating intents: ${err}`);
-        // });
+//     if (intent) {
+//     intentsClient
+//         .patch(request)
+//         // .then(responses => {
+//         //     console.log(`Intents updated: ${responses[0]}`);
+//         //     const getIntentRequest = { name: responses[0] };
+//         //     // Call the getIntent method
+//         //     console.log('==============================================================================================', intentsClient.getIntent(getIntentRequest));
+//         // })
+//         // .catch(err => {
+//         //     console.error(`Error updating intents: ${err}`);
+//         // });
+//     }
+//     if (entity){
+//     entitiesClient
+//         .patch(request2)
+//         // .then(responses => {
+//         //     console.log(`Intents updated: ${responses[0]}`);
+//         //     const getIntentRequest = { name: responses[0] };
+//         //     // Call the getIntent method
+//         //     console.log('==============================================================================================', intentsClient.getIntent(getIntentRequest));
+//         // })
+//         // .catch(err => {
+//         //     console.error(`Error updating intents: ${err}`);
+//         // });
+
+//     }
+//         res.redirect('/')
+// });
+app.get('/copy', async (req, res) => {
+
+ 
+    const data = fs.readFileSync(`./${req.query.upload}`, 'utf-8');
+    const jsonData = JSON.parse(data);
+
+    for (let i = 0; i < req.query.uploadArr;i++){
+        fs.writeFileSync(dirname+`./${req.query.uploadArr[i]}`, JSON.stringify(jsonData));
     }
-    if (entity){
-    entitiesClient
-        .patch(request2)
-        // .then(responses => {
-        //     console.log(`Intents updated: ${responses[0]}`);
-        //     const getIntentRequest = { name: responses[0] };
-        //     // Call the getIntent method
-        //     console.log('==============================================================================================', intentsClient.getIntent(getIntentRequest));
-        // })
-        // .catch(err => {
-        //     console.error(`Error updating intents: ${err}`);
-        // });
-
-    }
-        res.redirect('/')
-});
-
+   await res.redirect('/')
+})
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
